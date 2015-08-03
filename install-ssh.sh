69658e34d480219e8c0423d667dbe99a597231b3
Zimmer-web <getarun@users.noreply.github.com>
2015-08-03 15:20:31 +0200

    Update install-ssh.sh

--- install-ssh.sh
+++ install-ssh.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 -*- coding: utf-8 -*-
-sudo apt-get install openssh-server openssh-client
+echo "installing ssh-software"
+sudo apt-get install openssh-server openssh-client ssh-agent
 ssh-keygen
 echo "created key pair ~/.ssh/id_rsa"
+ssh-agent -s
+ssh-add ~/.ssh/id_rsa
+# ssh-copy-id getarun@23.45.56.78
+echo "testing connection"
+ssh -T git@github.com
