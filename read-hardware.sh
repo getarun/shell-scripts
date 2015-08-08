450bac1318010eddace4e3ebfb2327043bff9c02
Zimmer-web <getarun@users.noreply.github.com>
2015-08-08 22:40:36 +0200

    Update read-hardware.sh

--- read-hardware.sh
+++ read-hardware.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 dmidecode -t baseboard
+Mainboard auslesen: dmidecode -t baseboard
+
+BIOS auslesen: dmidecode -t bios
+
+Speicher auslesen: dmidecode -t memory
+
+Chassis auslesen: dmidecode -t chassis
+
+Prozessor auslesen: dmidecode -t processor
+
+Prozessorcache auslesen: dmidecode -t cache
+
+Mainboard Anschlüsse auslesen: dmidecode -t  connector
+
+PCI Slots auslesen: dmidecode -t slot
